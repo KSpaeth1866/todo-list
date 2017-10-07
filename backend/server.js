85ada37adfa3a938cebe6864bd101cdd9174c55d
Kobie <spaet062@umn.edu>
2017-10-06 23:21:31 -0700

    Setup server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.static('build'));
+
+const dbRoutes = require('./routes/databaseAccess.js');
+app.use('/db', dbRoutes);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("\n\nListening on port: ", port);
+})
